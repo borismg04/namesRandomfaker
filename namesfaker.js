35d66ae84f299cb11cee2264af24d506f7d247cd
borismg04 <borismg04@gmail.com>
2022-03-02 21:32:57 -0500

    feat:Correccion

--- namesfaker.js
+++ namesfaker.js
@@ -1,12 +1,15 @@
-const pack=require('fs');
-const { faker } = require('@faker-js/faker');
-let names=[];
+const pack = require("fs");
+const { faker } = require("@faker-js/faker");
+let names = [];
 
 for (let i = 0; i <= 1000; i++) {
     const randomName = faker.name.findName();
-    names.push('Number = '+i,"Name = "+randomName)
+    names.push("Number = " + i, "Name = " + randomName);
 }
 
-pack.writeFileSync('fakerRandom.txt',JSON.stringify(names));
-
-console.log('File fakerRandom.txt created');
+try {
+    pack.writeFileSync("fakerRandom.txt", JSON.stringify(names));
+    console.log("File fakerRandom.txt created");
+} catch (error) {
+    console.log("Failed");
+}
